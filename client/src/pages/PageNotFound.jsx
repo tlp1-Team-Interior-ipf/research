7fac33c1bfbf5d47a66f8f294efd4ada4551cbd8
Fabian <fabianvillagra28@gmail.com>
2023-11-11 20:01:13 -0300

    fix: pagina 404

--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -3,7 +3,10 @@ import React from "react";
 export const PageNotFound = () => {
     return (
         <div>
-            <h1>Page Not Found</h1>
+            <h1 className="d-flex justify-content-center mt-5">Page Not Found</h1>
+            <div className="d-flex justify-content-center">
+                <img src="/img/taxi.gif" alt="GIF de taxi" />
+            </div>
         </div>
     )
 }
