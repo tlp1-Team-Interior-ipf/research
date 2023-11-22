0b6deffb495d1a02feffb4e2d4dd2bfdba670864
Fausto <ofaustojoaquindejesus@gmail.com>
2023-11-22 15:19:59 -0300

    se agregó el boton de solicitar viaje

--- client/src/components/EmpresasDisponibles.jsx
+++ client/src/components/EmpresasDisponibles.jsx
@@ -1,3 +1,5 @@
+
+import React from 'react';
 import './empresa.css';
 
 export const EmpresasDisp = () => {
@@ -5,7 +7,7 @@ export const EmpresasDisp = () => {
         <main>
             <div>
                 <div className='d-flex justify-content-center'>
-                <img className="img-fluid" src="/img/image.png" alt="Imagen de taxi" height="300" width="300" />
+                    <img className="img-fluid" src="/img/image.png" alt="Imagen de taxi" height="300" width="300" />
                 </div>
                 <div className="letras">
                     <div className="album py-5 bg-body-tertiary">
@@ -17,9 +19,10 @@ export const EmpresasDisp = () => {
                                     <div className="card shadow-sm">
                                         <img src="/img/remises fontana.jpeg" alt="Remises Fontana" />
                                         <div className="card-body">
-                                
-                                            <text className="card-title">Remises Fontana</text>
+                                            <h5 className="card-title">Remises Fontana</h5>
                                             <p className="card-text">Pantaleon Gomez 636. C.P. P3600KYN, Formosa, Formosa.</p>
+                                            <button className="btn btn-primary">Solicitar Viaje</button>
+                                            <p className="monto-aproximado">Monto aproximado: $20</p>
                                         </div>
                                     </div>
                                 </div>
@@ -27,9 +30,10 @@ export const EmpresasDisp = () => {
                                     <div className="card shadow-sm">
                                         <img src="/img/remises libertad.webp" alt="Remises Libertad" id="libertad" />
                                         <div className="card-body">
-                                            <text className="card-title">Remises Libertad</text>
+                                            <h5 className="card-title">Remises Libertad</h5>
                                             <p className="card-text">Av Gutnisky 2133. C.P. P3600, Formosa, Formosa.</p>
-                                
+                                            <button className="btn btn-primary">Solicitar Viaje</button>
+                                            <p className="monto-aproximado">Monto aproximado: $25</p>
                                         </div>
                                     </div>
                                 </div>
@@ -37,9 +41,10 @@ export const EmpresasDisp = () => {
                                     <div className="card shadow-sm">
                                         <img src="/img/remises litur.png" alt="Remises Litur" />
                                         <div className="card-body">
-                                            <text className="card-title">Remises Litur</text>
+                                            <h5 className="card-title">Remises Litur</h5>
                                             <p className="card-text">Av. González Lelong 1025, P3600 DGB, Formosa</p>
-                                
+                                            <button className="btn btn-primary">Solicitar Viaje</button>
+                                            <p className="monto-aproximado">Monto aproximado: $18</p>
                                         </div>
                                     </div>
                                 </div>
@@ -47,9 +52,10 @@ export const EmpresasDisp = () => {
                                     <div className="card shadow-sm">
                                         <img src="/img/remises montecarlo.jpg" alt="Remises Montecarlo" />
                                         <div className="card-body">
-                                            <text className="card-title">Remises Montecarlo</text>
+                                            <h5 className="card-title">Remises Montecarlo</h5>
                                             <p className="card-text">Trinidad González 629, P3600 Formosa</p>
-                                
+                                            <button className="btn btn-primary">Solicitar Viaje</button>
+                                            <p className="monto-aproximado">Monto aproximado: $22</p>
                                         </div>
                                     </div>
                                 </div>
@@ -57,9 +63,10 @@ export const EmpresasDisp = () => {
                                     <div className="card shadow-sm">
                                         <img src="/img/remises napoleón.jpg" alt="Remises Napoleón" />
                                         <div className="card-body">
-                                            <text className="card-title">Remises Napoleón</text>
+                                            <h5 className="card-title">Remises Napoleón</h5>
                                             <p className="card-text">Av. Napoleón Uriburu 1645, P3600 DQY, Formosa.</p>
-                                
+                                            <button className="btn btn-primary">Solicitar Viaje</button>
+                                            <p className="monto-aproximado">Monto aproximado: $30</p>
                                         </div>
                                     </div>
                                 </div>
@@ -67,12 +74,14 @@ export const EmpresasDisp = () => {
                                     <div className="card shadow-sm">
                                         <img src="/img/remises norte.jpeg" alt="Remises Norte" id="remisesnorte" />
                                         <div className="card-body">
-                                            <text className="card-title">Remises Norte</text>
+                                            <h5 className="card-title">Remises Norte</h5>
                                             <p className="card-text">Pringles 165, P3600 Formosa</p>
-                                
+                                            <button className="btn btn-primary">Solicitar Viaje</button>
+                                            <p className="monto-aproximado">Monto aproximado: $28</p>
                                         </div>
                                     </div>
                                 </div>
+                                {/* Repite la estructura similar para otras tarjetas */}
                             </div>
                         </div>
                     </div>
