c52f954fcad859ac575845b3b457cfb5379ab045
Mauri Barrientos <mauribarrientos23@gmail.com>
2023-11-24 10:39:02 -0300

    Fix: to fixed

--- client/src/components/EmpresasDisponibles.jsx
+++ client/src/components/EmpresasDisponibles.jsx
@@ -12,51 +12,43 @@ export const EmpresasDisp = ({
   };
     return (
         <main>
-            <div>
-                <div className="letras">
-                    <div className="album py-5 bg-body-tertiary">
-                        <div className="container">
-                            <h2>Empresas disponibles</h2>
-                            <br />
-                            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                                <div className="col">
-                                    <div className="card shadow-sm">
-                                        <img src="/img/remises libertad.jpg" alt="Remises Libertad" className="card-img-top" /> 
-                                        <div className="card-body">
-                                            <h5 className="card-title">Remises Libertad</h5>
-                                            <p className="card-text">Av. Gutnisky 2133</p>
-                                            <button className="btn btn-primary" onClick={() => handleSolicitarViaje(1)}>Solicitar Viaje</button>
-                                            <p className="monto-aproximado">Monto aproximado: ${montoRealLibertad || 'Calculando...'}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col">
-                                    <div className="card shadow-sm">
-                                        <img src="/img/remises montecarlo.jpg" alt="Remises Montecarlo" />
-                                        <div className="card-body">
-                                            <h5 className="card-title">Remises Montecarlo</h5>
-                                            <p className="card-text">Trinidad González 629, P3600 Formosa</p>
-                                            <button className="btn btn-primary" onClick={() => handleSolicitarViaje(2)}>Solicitar Viaje</button>
-                                            <p className="monto-aproximado">Monto aproximado: ${montoRealMontecarlo || 'Calculando...'}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col">
-                                    <div className="card shadow-sm">
-                                        <img src="/img/remises napoleón.jpg" alt="Remises Napoleón" />
-                                        <div className="card-body">
-                                            <h5 className="card-title">Remises Napoleón</h5>
-                                            <p className="card-text">Av. Napoleón Uriburu 1645, P3600 DQY, Formosa.</p>
-                                            <button className="btn btn-primary" onClick={() => handleSolicitarViaje(3)}>Solicitar Viaje</button>
-                                            <p className="monto-aproximado">Monto aproximado: ${montoRealNapoleon || 'Calculando...'}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  <div className="letras">
+    <div className="album py-5 bg-body-tertiary">
+      <div className="container">
+        <h2>Empresas disponibles</h2>
+        <br />
+        <div className="card-grid">
+          <div className="card shadow-sm">
+            <img src="/img/remises libertad.jpg" alt="Remises Libertad" className="card-img-top" />
+            <div className="card-body">
+              <h5 className="card-title">Remises Libertad</h5>
+              <p className="card-text">Av. Gutnisky 2133</p>
+              <button className="btn btn-primary" onClick={() => handleSolicitarViaje(1)}>Solicitar Viaje</button>
+              <p className="monto-aproximado">Monto aproximado: ${montoRealLibertad.toFixed(2) || 'Calculando...'}</p>
             </div>
-        </main>
+          </div>
+          <div className="card shadow-sm">
+            <img src="/img/remises montecarlo.jpg" alt="Remises Montecarlo" className="card-img-top" />
+            <div className="card-body">
+              <h5 className="card-title">Remises Montecarlo</h5>
+              <p className="card-text">Trinidad González 629, P3600 Formosa</p>
+              <button className="btn btn-primary" onClick={() => handleSolicitarViaje(2)}>Solicitar Viaje</button>
+              <p className="monto-aproximado">Monto aproximado: ${montoRealMontecarlo || 'Calculando...'}</p>
+            </div>
+          </div>
+          <div className="card shadow-sm">
+            <img src="/img/remises napoleón.jpg" alt="Remises Napoleón" className="card-img-top" />
+            <div className="card-body">
+              <h5 className="card-title">Remises Napoleón</h5>
+              <p className="card-text">Av. Napoleón Uriburu 1645, P3600 DQY, Formosa.</p>
+              <button className="btn btn-primary" onClick={() => handleSolicitarViaje(3)}>Solicitar Viaje</button>
+              <p className="monto-aproximado">Monto aproximado: ${montoRealNapoleon || 'Calculando...'}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
     );
 }
