7d44231c069025c9d56c4d8081a57065d7ad8254
Fausto <ofaustojoaquindejesus@gmail.com>
2023-11-23 21:54:58 -0300

    se establecieron las relaciones

--- server/src/models/passenger.js
+++ server/src/models/passenger.js
@@ -1,70 +1,76 @@
 import { sequelize } from '../config/db.js';
+import { TravelModel } from './travel.js';
 import { DataTypes } from 'sequelize';
 
 
-
-// Database modeling for the User entity
 export const PassengerModel = sequelize.define('Passenger', {
-        // Model attributes are defined here
-        id: {
-          type: DataTypes.INTEGER,
-          primaryKey: true,
-          autoIncrement: true
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    surname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    phone_number: {
+        type: DataTypes.STRING, 
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: {
+            args: true,
+            message: 'Email already exists' 
         },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        surname: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        phone_number: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: {
-                args: true,
-                messge: 'Email already exists'
-            },
-        },
-        date_birth: {
-            type: DataTypes.DATE,
-            allowNull: true
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        estado: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: true
-        },
-        userType: {
-            type: DataTypes.INTEGER,
-            allowNull: true
-        },
-        createdAt: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
-        },
-        updatedAt: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
-        },
-        deletedAt: {
-            type: DataTypes.DATE,
-            allowNull: true
-        }
-    }, {
-        // Other model options go here
-        createdAt: true,
-        updatedAt: true,
-        deletedAt: true,
-        tableName: 'Passenger'
+    },
+    date_birth: {
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    estado: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+    },
+    userType: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    deletedAt: {
+        type: DataTypes.DATE,
+        allowNull: true
+    }
+}, {
+    createdAt: true,
+    updatedAt: true,
+    deletedAt: true,
+    tableName: 'Passenger'
+});
+
+// Establecer la relación de 1 a 1 entre Passenger y Travel
+PassengerModel.hasOne(TravelModel, {
+    foreignKey: 'passengerId'
+});
+
+TravelModel.belongsTo(PassengerModel, {
+    foreignKey: 'passengerId'
 });
--- server/src/models/travel.js
+++ server/src/models/travel.js
@@ -1,32 +1,40 @@
 import { sequelize } from '../config/db.js';
-import { Sequelize } from 'sequelize';
+import { EnterpriseModel } from '../models/enterprise.js';
+import { DataTypes } from 'sequelize';
 
 export const TravelModel = sequelize.define('Travel', {
-  id_enterprise:{
-    type: Sequelize.INTEGER,
-    allowNull: false
-  },
-  origen_lat: {
-    type: Sequelize.DECIMAL (12,10),
-    allowNull: false
-  },
-  origen_lng: {
-    type: Sequelize.DECIMAL (12,10),
-    allowNull: false
-  },
-  destino_lat: {
-    type: Sequelize.DECIMAL (12,10),
-    allowNull: false
-  },
-  destino_lng: {
-    type: Sequelize.DECIMAL (12,10),
-    allowNull: false
-  }},
-  {
-    // Other model options go here
+    id_enterprise: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    origen_lat: {
+        type: DataTypes.DECIMAL(12, 10),
+        allowNull: false
+    },
+    origen_lng: {
+        type: DataTypes.DECIMAL(12, 10),
+        allowNull: false
+    },
+    destino_lat: {
+        type: DataTypes.DECIMAL(12, 10),
+        allowNull: false
+    },
+    destino_lng: {
+        type: DataTypes.DECIMAL(12, 10),
+        allowNull: false
+    }
+}, {
     createdAt: true,
     updatedAt: true,
     deletedAt: true,
     tableName: 'Travel'
 });
 
+// Establecer la relación entre Travel y Enterprise
+TravelModel.belongsTo(EnterpriseModel, {
+    foreignKey: 'id_enterprise'
+  });
+
+EnterpriseModel.hasMany(TravelModel, {
+    foreignKey: 'id_enterprise'
+});
