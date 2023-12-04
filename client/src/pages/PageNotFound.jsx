6ef1398fd21269dbbe050ac88a4876d5f2c0daf0
Fabian <fabianvillagra28@gmail.com>
2023-12-04 09:43:14 -0300

    fix: cambios en la lógica de la pagina de not found

--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
+
 
 export const PageNotFound = () => {
+  if (localStorage.getItem("token")) {
+    return <Navigate to="/home" />;
+  } else {
     return (
-        <div>
-            <h1 className="d-flex justify-content-center mt-5">Page Not Found</h1>
-            <div className="d-flex justify-content-center">
-                <img src="/img/taxi.gif" alt="GIF de taxi" />
-            </div>
+        <>
+      <div>
+        <h1 className="d-flex justify-content-center mt-5">Page Not Found</h1>
+        <div className="d-flex justify-content-center">
+          <img src="/img/taxi.gif" alt="GIF de taxi" />
         </div>
-    )
-}
+      </div>
+        <div className="d-flex justify-content-center mt-5">
+        <button className="btn btn-warning">
+            <a href="/" className="text-black">Volver al inicio</a>
+        </button>
+        </div>
+        </>
+    );
+  }
+};
