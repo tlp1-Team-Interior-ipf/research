d88115508db6fa00e5bd5d713e0ee0e3ff274e0b
Fabian <fabianvillagra28@gmail.com>
2023-12-06 12:28:37 -0300

    fix: botones del login y redirección del boton del index

--- client/src/components/Index.jsx
+++ client/src/components/Index.jsx
@@ -6,7 +6,7 @@ export const Index = () => {
           <div className="letras">
           <h1 className="display-1">tuRemo</h1>
           <h3 className="display-6">¿Necesitas un remis?</h3>
-          <a className="btn btn-warning d-grid gap-2 col-6 mx-auto" href="/home/usuario" type="button">Pedir Aquí</a>
+          <a className="btn btn-warning d-grid gap-2 col-6 mx-auto" href="/inicioSesion" type="button">Pedir Aquí</a>
         </div>
         </div>
       </main>
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -5,9 +5,9 @@ export const Login = () => {
           <img className="img-fluid" src="/img/image.png" alt="Imagen de taxi" height="300" width="300"/>
             <div className="col-6 mx-auto">
               <h5>Elige cómo quieres iniciar sesión</h5>
-              <div className="d-flex justify-content-around">
+              <div className="d-flex">
               <a href="/inicioSesion"><button className="btn btn-warning"type="button">Pasajero</button></a>
-              <a href="/inicioSesionChofer"><button className="btn btn-warning" type="button">Chofer</button></a>
+              <a href="/inicioSesionChofer"><button className="btn btn-warning mx-3" type="button">Chofer</button></a>
               </div>
             </div>
           </div>
